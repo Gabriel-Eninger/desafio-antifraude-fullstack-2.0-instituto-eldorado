66132b13c92163a171404258bd8fe9ea5e972ee1
Joaquim Teixeira <tjoaquim.sm@gmail.com>
2026-07-29 16:54:54 -0300

    adicionamos a regra de urgencia e a função para normalizar as mensagens

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,17 +1,31 @@
+import java.text.Normalizer;
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main{
     public static int score = 0;
-    public static String regra_urgencia(String mensagem) {
-
-        return mensagem;
-    }
-    public static void main(String[] args){
+    public static void  inputMensagem(){
         Scanner scanner= new Scanner(System.in);
         System.out.println("Digite sua mensagem:");
         String mensagem = scanner.nextLine();
-        String lowerCase = mensagem.toLowerCase();
-        System.out.println(lowerCase);
+        mensagem = mensagem.toLowerCase();
+        mensagem = mensagem.replaceAll("[!.,?;-]", "");
+        mensagem = removerAcentos(mensagem);
+    }
+    public static String removerAcentos(String str) {
+        return Normalizer.normalize(str, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+    }
+    public static void regra_urgencia(String mensagem) {
+        String[] flags = {"urgente", "urgencia", "emergencia", "urgentemente"};
+        if (Arrays.stream(flags).anyMatch(mensagem::contains)) {
+            score += 1;
+        }
+    }
+    public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println(score);
+
+
+
     }
 }
