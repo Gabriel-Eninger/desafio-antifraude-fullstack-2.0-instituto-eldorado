95adf44b1b19c10c15e1d0852976c80039314187
Joaquim <tjoaquim.sm@gmail.com>
2026-07-30 16:24:53 -0300

    adição do remetente na função regra_urgencia

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,29 +3,39 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main{
+    static Scanner scanner= new Scanner(System.in);
+    // VARIAVEIS
+    public static boolean remetente;
     public static int score = 0;
-    public static void  inputMensagem(){
-        Scanner scanner= new Scanner(System.in);
-        System.out.println("Digite sua mensagem:");
-        String mensagem = scanner.nextLine();
-        mensagem = mensagem.toLowerCase();
-        mensagem = mensagem.replaceAll("[!.,?;-]", "");
-        mensagem = removerAcentos(mensagem);
-    }
+    public static String mensagem;
+    // FUNCOES
+
     public static String removerAcentos(String str) {
         return Normalizer.normalize(str, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+    }
+
+    public static void  inputMensagem(){
+        System.out.println("Digite sua mensagem:");
+        mensagem = scanner.nextLine();
+        mensagem = mensagem.toLowerCase().replaceAll("[!.,?;-]", "");
+        mensagem = removerAcentos(mensagem);
     }
     public static void regra_urgencia(String mensagem) {
         String[] flags = {"urgente", "urgencia", "emergencia", "urgentemente"};
         if (Arrays.stream(flags).anyMatch(mensagem::contains)) {
             score += 1;
         }
+        System.out.println("Você conhece o remetente desta mensagem? (responda com sim ou nao)");
+        String resposta = scanner.nextLine().trim().toLowerCase();
+        boolean remetente = resposta.equals("sim");
+        if (!remetente) {score += 1;}
     }
+    // SAIDA
     public static void main(String[] args){
-        Scanner scanner= new Scanner(System.in);
+        System.out.println("Bem vindo ao autenticador de segurança");
+        inputMensagem();
+        regra_urgencia(mensagem);
         System.out.println(score);
-
-
 
     }
 }
